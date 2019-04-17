94aa33d78b345352ec0baebb76045730b08ad4b7
ilyoss <ily.trevino@live.com>
2019-04-17 11:02:47 -0500

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+
+    absolutePath: function (absolutePath){
+      return path.isAbsolute(absolutePath);
+    },
+    relativePath: function (relativePath) {
+      return path.resolve(path.join(__dirname, "..", relativePath));
+    },
+    existingPath: function(inputPath){
+      let exists = fs.existsSync(inputPath);
+      return exists;
+    },
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,41 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../index.js');
 
 describe('mdLinks', () => {
+  test('is an object', () => {
+   expect(typeof mdLinks).toBe('object');
+  });
+});
 
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('absolutePath', () => {
+  it('Should be a function', () => {
+    expect(typeof mdLinks.absolutePath).toBe('function');
   });
 
+  it('Should validate if path is absolute', () => {
+    expect(mdLinks.absolutePath('/test/demo_path.js')).toBe(true);
+  });
+});
+
+describe('relativePath', () => {
+  it('Should be a function', () => {
+    expect(typeof mdLinks.relativePath).toBe('function');
+  });
+
+  it('Should turn path to absolute', () => {
+    expect(mdLinks.relativePath('files')).toBe('');
+  });
+});
+
+describe('existingPath', () => {
+  it('Should be a function', () => {
+    expect(typeof mdLinks.existingPath).toBe('function');
+  });
+
+  it('Should validate if path is a valid directory or file', () => {
+    expect(mdLinks.existingPath('c:\\dog.jpg')).toBe(false);
+  });
+
+  it('Should validate if path is a valid directory or file', () => {
+    expect(mdLinks.existingPath('\\Downloads\\chocolate-doom-3.0.0-win32')).toBe(true);
+  });
 });
